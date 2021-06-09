d2810e96c279cc47c1edd8b0b90f741f315ffa14
Mykyta <sinegibskij@gmail.com>
2021-06-09 14:36:05 +0300

    scripts

--- script/script.js
+++ script/script.js
@@ -1,13 +1,22 @@
-let numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
 
 const PersonalMovieDb = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false;
+    privat: false
 }
 
-let lastOfFilms1 = prompt('Один из последних просмотренных фильмов?', '');
+let lastOfFilms1 = prompt('Один из последних просмотренных фильмов?', ''),
+lastOfFilms2 = prompt('Один из последних просмотренных фильмов?', '');    
 
-let lastOfFilms2 = prompt('Один из последних просмотренных фильмов?', '');    
+let rate1 = prompt('на сколько оцените его?', ''),
+    rate2 = prompt('на сколько оцените его?', '');
+
+
+
+PersonalMovieDb.movies[lastOfFilms1] = rate1;
+PersonalMovieDb.movies[lastOfFilms2] = rate2;
+
+console.log(PersonalMovieDb)
