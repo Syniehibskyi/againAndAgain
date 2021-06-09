11bc2017d8c225ca72051ca9674b1761268cb9a7
Mykyta <sinegibskij@gmail.com>
2021-06-09 14:12:39 +0300

    script added

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
+let numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
 
+const PersonalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false;
+}
 
+let lastOfFilms1 = prompt('Один из последних просмотренных фильмов?', '');
 
-    
+let lastOfFilms2 = prompt('Один из последних просмотренных фильмов?', '');    
